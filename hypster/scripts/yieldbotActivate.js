0935dcfa0a7ae39b216e1526314023a666601249
John Shearer <john@ultranaut.com>
2017-01-19 15:17:11 -0500

    Move YB load and activate code back into the banner ad

--- hypster/scripts/yieldbotActivate.js
+++ hypster/scripts/yieldbotActivate.js
@@ -1,12 +1,11 @@
 /**
  * Yieldbot.com Intent Tag ACTIVATION
  */
-ybotq.push(function () {
-  yieldbot.pub('320d');
-  yieldbot.defineSlot('LB');
-  yieldbot.defineSlot('MR');
-  yieldbot.defineSlot('MR_BTF');
-  yieldbot.defineSlot('LB_BTF');
-  yieldbot.enableAsync();
-  yieldbot.go();
-});
+/*
+yieldbot.pub('320d');
+yieldbot.defineSlot('LB');
+yieldbot.defineSlot('MR');
+yieldbot.defineSlot('MR_BTF');
+yieldbot.defineSlot('LB_BTF');
+yieldbot.go();
+*/
--- hypster/scripts/yieldbotLoad.js
+++ hypster/scripts/yieldbotLoad.js
@@ -1,10 +1,11 @@
 /**
  * Yieldbot.com Intent Tag LOADING
  */
-var ybotq = ybotq || [];
+/*
 (function() {
   var js = document.createElement('script');
   js.src = '//cdn.yldbt.com/js/yieldbot.intent.js';
   var node = document.getElementsByTagName('script')[0];
   node.parentNode.insertBefore(js, node);
 })();
+*/
