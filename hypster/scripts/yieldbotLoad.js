27b3e605dd9820c084f3e850fcb6391539d697b9
John Shearer <john@ultranaut.com>
2017-01-19 15:31:41 -0500

    Move YB load call out of 728x90

--- hypster/scripts/yieldbotLoad.js
+++ hypster/scripts/yieldbotLoad.js
@@ -1,11 +1,9 @@
 /**
  * Yieldbot.com Intent Tag LOADING
  */
-/*
 (function() {
   var js = document.createElement('script');
   js.src = '//cdn.yldbt.com/js/yieldbot.intent.js';
   var node = document.getElementsByTagName('script')[0];
   node.parentNode.insertBefore(js, node);
 })();
-*/
